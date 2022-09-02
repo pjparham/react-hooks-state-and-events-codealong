4909faf22e0fa4ad5c2db72acb3af9c9981407ad
Parker Parham <parkerparham@gmail.com>
2022-09-02 11:55:25 -0400

    commit tree 9b84cfcb0fb8de70dd9094b89e45e8934a832f9e

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, changeIsOn] = useState(false)
+
+  function setButton(){
+    changeIsOn((isOn) => !isOn)
+  }
+
+  const color = isOn ? "red" : "yellow"
+
+  return <button style={{ background: color}} onClick={setButton}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
